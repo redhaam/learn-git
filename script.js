729c1993a1edaaeda8eadd2b9a7879bfb3848774
Hichma <hichemsidi58@gmail.com>
2022-01-29 11:50:42 +0100

    dark Theme

--- script.js
+++ script.js
@@ -2,18 +2,12 @@
 
 let toggleButton = document.querySelector("#toggle");
 
-let darkTheme = false;
-
 let htmlBody = document.body;
 
 toggleButton.addEventListener("click", switchToDarkTheme);
 
-function switchToDarkTheme(params) {
-    if (darkTheme = false) {
-        htmlBody.classList.add("darkTheme")
-    }else if (darkTheme) {
-        htmlBody.classList.remove("darkTheme")
-    }
+function switchToDarkTheme() {
+   htmlBody.classList.toggle("darkTheme")
 }
 
 // Hichem's script's end
