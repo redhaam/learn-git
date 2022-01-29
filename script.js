0218ffab1f533db5b982dba152cef6cf74fe05ed
Hichma <hichemsidi58@gmail.com>
2022-01-29 11:44:57 +0100

    Added Dark Theme Toggle

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+// Hichem's script
+
+let toggleButton = document.querySelector("#toggle");
+
+let darkTheme = false;
+
+let htmlBody = document.body;
+
+toggleButton.addEventListener("click", switchToDarkTheme);
+
+function switchToDarkTheme(params) {
+    if (darkTheme = false) {
+        htmlBody.classList.add("darkTheme")
+    }else if (darkTheme) {
+        htmlBody.classList.remove("darkTheme")
+    }
+}
+
+// Hichem's script's end
