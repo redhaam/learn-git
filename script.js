da06938030bc3ce82a00d94840b1575eeb0e5416
Redha Amirouche <hm_amirouche@esi.dz>
2022-01-29 11:55:13 +0100

    Add quiz game page

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello world");
+
+const solution = "git commit";
+const feedback = document.querySelector("#feedback");
+
+document.quiz.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userAnswer = document.quiz.answer.value;
+  if (userAnswer.toLowerCase() == solution.toLocaleLowerCase()) {
+    feedback.textContent = "you solved our quiz congrats!!";
+  } else {
+    feedback.textContent = " wrong answer ! try again";
+  }
+});
